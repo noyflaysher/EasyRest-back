2ad60d4429e7532f5199259b22d47582f88ec21c
maya koma <mayakoma@gmail.com>
2023-06-04 19:37:11 +0300

    update dish es time

--- Routes/closeTableRoute.js
+++ Routes/closeTableRoute.js
@@ -6,5 +6,5 @@ const router = express.Router();
 router.post("/payment", closeTableController.payment);
 router.get("/getCloseTables", closeTableController.GetAllCloseTables);
 router.get("/RemoveAll", closeTableController.RemoveAll);
-
+//
 module.exports = router;
