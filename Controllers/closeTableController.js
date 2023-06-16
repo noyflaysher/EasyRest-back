0e8ff4b8dd2a04e4a21a57eb51020a73e4812943
Noy Flaysher <noyflasyehr12@gmail.com>
2023-06-16 15:38:30 +0300

    make it better

--- Controllers/closeTableController.js
+++ Controllers/closeTableController.js
@@ -25,7 +25,7 @@ const payment = async (req, res, next) => {
     return next(error);
   }
 
-  if (opentbl === undefined || !opentbl || opentbl.length == 0) {
+  if (!opentbl || opentbl.length == 0) {
     const error = new HttpError('Table is not exist', 500);
     return next(error);
   }
